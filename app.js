8a3ec31fe2e20372e84fdaa97c3d469a7f8c6ae9
RNU4 <malt2592@gmail.com>
2023-11-24 01:39:34 +0100

    more clean up and minimizing of unnecessary code

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var box = document.getElementById("box");
 var input = document.getElementById("input");
+var answer = document.getElementById("answer")
 var word_answer = ""
-var empty_char = " "
+const empty_char = " "
+const max_row = 6
+const word_max = 5
 var current_row = 0;
-var max_row = 6
-var word_max = 5
 var word_map = {};
 var word_keys = [];
 const file_path = "./assets/valid-wordle-words.txt";
@@ -18,7 +19,7 @@ fetch(file_path)
         word_map[lines[i].trim()] = 1;
     }
     word_keys = Object.keys(word_map)
-    pick_word() // pick first word
+    new_word() // pick first word
 }).catch(error => console.error('Error fetching or reading file:', error.message));
 }get_data();
 
@@ -27,11 +28,6 @@ for (var i = 0; i< max_row; i++){
         box.innerHTML+=`<p id = 'p${i}_${j}' class='word'>${empty_char}</p>` 
     }
     box.innerHTML+="<p class = 'spacer'></p>"
-}
-
-function pick_word(){
-    word_answer = word_keys[Math.floor(Math.random() * word_keys.length)]
-    console.log("Answer : "+word_answer)
 }
 
 function new_word(){ //remove answers
@@ -43,65 +39,54 @@ function new_word(){ //remove answers
             element.style.color = "white"
         }
     }
-    document.getElementById("answer").textContent = " "
     current_row = 0; //reset row
-    pick_word()     //new word
+    answer.textContent = empty_char
+    word_answer = word_keys[Math.floor(Math.random() * word_keys.length)]
+    console.log("Answer : "+word_answer)
 }
-
 
 input.addEventListener('keydown', function (event) {
     if (event.keyCode == 13) { // check enter key
-        set_answer();
+        if (current_row != max_row && answer.textContent == empty_char)set_answer() //still tries left
+        input.value = ""
     }
 });
 
-function loser(){
-    answer = document.getElementById("answer")
-    answer.textContent = " Right answer was "+word_answer
-    current_row = max_row+1; // already lost so no more tries
-}
-function winner(){
-    current_row = max_row+1; // already won so no more tries
-    answer = document.getElementById("answer")
-    answer.textContent = "You guessed correct!"
+function inform_user(str){
+    answer.textContent = str
+    //current_row = max_row+1; // already lost so no more tries
 }
 
 function set_answer(){
-    if (current_row == max_row+1){return} // all tries used
-
     //is real word? 
     if (!(input.value in word_map)){
         console.log("not real word\n")
-        input.value = ""
         return
     }
 
     for(var i = 0; i< word_max; i++){ //place answer into boxes
-        var tex = document.getElementById(`p${current_row}_${i}`);
-        tex.textContent = input.value[i];
+        document.getElementById(`p${current_row}_${i}`).textContent = input.value[i];
     }
     
-    check_answer() //color it
+    color_answer();current_row++; //color it and go to next row
 
     if (input.value == word_answer){
-        winner()
-    }else if (current_row == max_row){ // u lose
-        loser()
+        inform_user("You guessed correct!")
+    }else if (current_row == max_row){ // you lose
+        inform_user("Right answer was "+word_answer)
     }
-    input.value = ""
 }
 
-function check_answer(){
+function color_answer(){
     for(var i = 0; i < word_max; i++){
         var element = document.getElementById(`p${current_row}_${i}`);
-        if (element.textContent==word_answer[i]){ //green
+        if (element.textContent==word_answer[i]){ //green (letter is right position)
             element.style.backgroundColor = "limegreen";
-        }else if (word_answer.includes(element.textContent)){ //yellow
+        }else if (word_answer.includes(element.textContent)){ //yellow (contains letter)
             element.style.backgroundColor = "sandybrown";
         }else {
-            element.style.backgroundColor = "lightgray";
+            element.style.backgroundColor = "lightgray"; //does not contain letter
         }
         element.style.color = "black" // make char visible
     }
-    current_row++;
 }
